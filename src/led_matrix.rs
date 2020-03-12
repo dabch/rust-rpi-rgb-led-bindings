654fa5e1df6eae0cf1c3a49a5ebc244068608b7a
Daniel Bücheler <dabch@posteo.de>
2020-03-12 21:42:29 +0000

    refactor to remove RGB, implement LedMatrix wrapper

--- src/led_matrix.rs
+++ src/led_matrix.rs
@@ -1,20 +1,21 @@
 mod led_matrix_c;
 
-use led_matrix_c::{RGBLedMatrix, RGBLedMatrixOptions, LedCanvas};
-//use led_matrix_c::{RGBLedMatrix, RGBLedMatrixOptions};
-use led_matrix_c::{led_matrix_create_from_options, led_matrix_get_canvas};
+pub use led_matrix_c::LedMatrixOptions;
+use led_matrix_c as c;
 use ::std::os::raw::{c_char, c_int};
 
 pub struct LedMatrix {
-    handle: *mut RGBLedMatrixOptions,
+    handle: *mut c::LedMatrix,
     options: LedMatrixOptions,
 }
 
-pub struct Led
+pub struct LedCanvas {
+    handle: *mut c::LedCanvas,
+}
 
 impl LedMatrixOptions {
-    pub fn new() -> RGBLedMatrixOptions {
-        RGBLedMatrixOptions {
+    pub fn new() -> LedMatrixOptions {
+        LedMatrixOptions {
             hardware_mapping: 0 as *const c_char,
             rows: 32,
             cols: 64,
@@ -30,29 +31,48 @@ impl LedMatrixOptions {
             led_rgb_sequence: 0 as *const c_char,
             pixel_mapper_config: 0 as  *const c_char,
             panel_type: 0 as  *const c_char,
-            _bitfield_1: RGBLedMatrixOptions::new_bitfield_1(0, 0, 0),
+            _bitfield_1: LedMatrixOptions::new_bitfield_1(0, 0, 0),
             __bindgen_padding_0: [0; 3],
         }
     }
 }
 
-impl RGBLedMatrix {
-    pub fn new(options: &mut RGBLedMatrixOptions) -> *mut RGBLedMatrix {
-        unsafe{
-            let ptr = led_matrix_create_from_options(options as *mut RGBLedMatrixOptions, 0 as *mut c_int, 0 as *mut*mut*mut c_char);
-            println!("matrix ptr: {:p}", ptr);
-            let mut m = *ptr;
-            println!("matrix ptr II: {:p}", &mut m as *mut RGBLedMatrix);
-            ptr
-        }
+impl LedMatrix {
+    pub fn new(mut options: LedMatrixOptions) -> LedMatrix {
+        let handle = 
+            unsafe{
+                let ptr = c::led_matrix_create_from_options(&mut options as *mut c::LedMatrixOptions, 0 as *mut c_int, 0 as *mut*mut*mut c_char);
+                println!("matrix ptr: {:p}", ptr);
+                let mut m = *ptr;
+                //println!("matrix ptr II: {:p}", &mut m as *mut c::LedMatrix);
+                ptr
+            };
+        LedMatrix { handle, options }
     }
 
-    pub fn get_canvas(matrix: *mut RGBLedMatrix) -> LedCanvas {
-        unsafe {
-            println!("starting out, matrix: {:p}...", matrix as *mut RGBLedMatrix);
-            let ptr = led_matrix_get_canvas(matrix as *mut RGBLedMatrix);
+    pub fn canvas(&self) -> LedCanvas {
+        let handle = unsafe {
+            println!("starting out, matrix: {:p}...", self.handle);
+            let ptr = c::led_matrix_get_canvas(self.handle);
             println!("got ptr: {:p}", ptr);
-            *ptr
-        }
+            ptr
+        };
+        LedCanvas { handle }
     }
+
+    pub fn offscreen_canvas(&self) -> LedCanvas {
+        let handle = unsafe {
+            let ptr = c::led_matrix_create_offscreen_canvas(self.handle);
+            ptr
+        };
+        LedCanvas { handle }
+    }
+
+    pub fn swap(&self, canvas: LedCanvas) -> LedCanvas {
+        let new_handle = unsafe {
+            c::led_matrix_swap_on_vsync(self.handle, canvas.handle)
+        };
+        LedCanvas { handle: new_handle }
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,17 @@
 //include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 mod led_matrix;
 
-use led_matrix::{RGBLedMatrixOptions, RGBLedMatrix};
+use led_matrix::{LedMatrixOptions, LedMatrix};
 use ::std::os::raw::{c_char, c_int};
 use std::{thread, time};
 
 fn main() {
     println!("Hello, world!");
-    /*let mut options = RGBLedMatrixOptions {
-        hardware_mapping: 0 as *const ::std::os::raw::c_char,
-        rows: 32,
-        cols: 64,
-        chain_length: 0,
-        parallel: 0,
-        pwm_bits: 0,
-        pwm_lsb_nanoseconds: 0,
-        pwm_dither_bits: 0,
-        brightness: 0,
-        scan_mode: 0,
-        row_address_type: 0,
-        multiplexing: 0,
-        led_rgb_sequence: 0 as *const ::std::os::raw::c_char,
-        pixel_mapper_config: 0 as  *const ::std::os::raw::c_char,
-        panel_type: 0 as  *const ::std::os::raw::c_char,
-        _bitfield_1: RGBLedMatrixOptions::new_bitfield_1(0, 0, 0),
-        __bindgen_padding_0: [0; 3],
-    };
-    */
-    let mut options = RGBLedMatrixOptions::new();
+    let mut options = LedMatrixOptions::new();
 
-    let mut matrix = RGBLedMatrix::from_options(&mut options);
+    let mut matrix = LedMatrix::new(options);
 
-    let mut canvas = RGBLedMatrix::get_canvas(matrix);
-    //let mut canvas = matrix.get_canvas();
+    let mut canvas = matrix.canvas();
 
     println!("done!");
 
